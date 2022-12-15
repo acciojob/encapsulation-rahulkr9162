574722c2f6ced29b110c4843ede01e6280e30ad1
rahulkr9162 <rahulkr9162@gmail.com>
2022-12-15 11:59:28 +0530

    message

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly obj = new RWOnly();
+        //obj.name = "Rahul";
+        //name has private access in com.driver.RWOnly
+        obj.setName("Rahul");
+        System.out.println(obj.getName());
+    }
   
 }
